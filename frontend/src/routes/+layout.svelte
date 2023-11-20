62cfb5543daf1299b00aef35c9109acd67579cc2
LefevreTom <lefevre.tomtom@gmail.com>
2023-11-20 09:56:39 +0100

    Added layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <h1>This is the header</h1>
+	<a href="/">Home</a>
+	<a href="/">About</a>
+	<a href="/">Settings</a>
+</nav>
+
+<slot></slot>
+
+<nav>
+    <h1>This is the footer</h1>
+	<a href="/">Link1</a>
+	<a href="/">Link2</a>
+	<a href="/">Link3</a>
+</nav>
