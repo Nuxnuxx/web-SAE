a8a88bfb7a6634e173d87911915790126e73658a
Nuxnuxx <nuxnux02290@gmail.com>
2023-11-20 11:38:39 +0100

    feat: add logging with morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import morgan from "morgan"
 import recipeRouter from "./api/v1/router/recipe.router.js"
 
 const app = express()
+
+app.use(morgan("dev"))
 
 app.use("/api/v1/recipe", recipeRouter)
 
