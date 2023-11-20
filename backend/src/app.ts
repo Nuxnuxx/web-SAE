cd7f74d1d0e382d9211a027819e19e00cfe835ce
Nuxnuxx <nuxnux02290@gmail.com>
2023-11-20 12:12:20 +0100

    feat: cors / JSON only and disable x powered by

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,22 @@
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
 import recipeRouter from "./api/v1/router/recipe.router.js"
 
 const app = express()
 
+app.disable('x-powered-by')
+
 app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+  }),
+)
 
 app.use("/api/v1/recipe", recipeRouter)
 
