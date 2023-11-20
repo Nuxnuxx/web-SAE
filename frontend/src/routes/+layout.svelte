5b485f3c2393699603dc54b96e679b421e5df159
LefevreTom <lefevre.tomtom@gmail.com>
2023-11-20 16:17:17 +0100

    Card component for mobile DONE.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -13,3 +13,24 @@
 	<a href="/">Link2</a>
 	<a href="/">Link3</a>
 </nav>
+<!-- 
+<style global>
+	@font-face {
+		font-family: "Inter";
+		src:
+			url("/fonts/Inter/Inter-VariableFont.ttf") format("truetype"),
+		}
+	@font-face {
+		font-family: "Leckerli One";
+		src:
+			url("/fonts/Leckerli_One/LeckerliOne-Regular.ttf") format("truetype"),
+	}
+    *{
+        font-family: "Inter", sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+        color: #000;
+        background-color: #fff;
+    }
+
+</style> -->
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,5 +2,25 @@
     <title>Accueil | PirateCook</title>
 </svelte:head>
 
+<script>
+    import RecipeCard from '../components/recipeCard.svelte';
+</script>
+
+<h2>Test liste éléments : </h2>
+<div class="list">
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+</div>
+<style>
+    .list{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 10px;
+        grid-auto-rows: minmax(100px, auto);
+    }
+</style>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
