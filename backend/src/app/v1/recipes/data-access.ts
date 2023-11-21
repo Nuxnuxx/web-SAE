1282077e06e4a38fa44f8a23f4e49ff404947ce3
Nuxnuxx <nuxnux02290@gmail.com>
2023-11-21 12:08:37 +0100

    fix: use non deprecated function to find by id

--- backend/src/app/v1/recipes/data-access.ts
+++ backend/src/app/v1/recipes/data-access.ts
@@ -7,17 +7,23 @@ export const getRecipesInDatabaseWithPagination = async (page_p: number) => {
 	const query = "MATCH (n:Recipe) RETURN n SKIP $page LIMIT $limit";
 	// float by default if not using .int on params
 	const currentPage = neo4j.int(page_p * PRODUCT_PER_PAGE);
+
 	const raw = await database.run(query, {
 		page: currentPage,
 		limit: neo4j.int(PRODUCT_PER_PAGE),
 	});
+
 	const result = raw.records.map((record) => record.get(0).properties);
+
 	return result;
 };
 
 export const getRecipesByIdInDatabase = async (id: number) => {
-	const query = "MATCH (n) WHERE id(n) = $nodeId RETURN n";
-	const result = await database.run(query, { nodeId: id });
+	const query = "MATCH (n) WHERE n.idRecipe = $idRecipe RETURN n";
+	// need a string to match the data in the database
+	const idString: string = String(id);
+	const result = await database.run(query, { idRecipe: idString });
+
 	return result;
 };
 
