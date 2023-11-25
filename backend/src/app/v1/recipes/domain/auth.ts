e1af831d1c0e81efe31543920000ad0ea41f9ab8
Nuxnuxx <nuxnux02290@gmail.com>
2023-11-25 13:53:27 +0100

    fix: delete files at wrong folder expand list enum http code

--- backend/src/app/v1/recipes/domain/auth.ts
+++
@@ -1,4 +0,0 @@
-export const findUser = (email: string) => {
-	//TODO: find user in database via search by email
-	return { name: "Wow", email: "test@gmail.com", password: "Test02290." };
-};
--- backend/src/libraries/error-handling/app-error.ts
+++ backend/src/libraries/error-handling/app-error.ts
@@ -28,4 +28,6 @@ export enum HttpStatusCode {
 	BAD_REQUEST = 400,
 	NOT_FOUND = 404,
 	INTERNAL_SERVER = 500,
+	CONFILCT = 409,
+	UNAUTHORIZED = 401,
 }
