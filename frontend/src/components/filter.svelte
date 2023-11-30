ddc07576f1e999039708e0d2f3f02166e445b115
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2023-11-30 11:37:21 +0100

    Component filter (starting) need to make the css

---
+++ frontend/src/components/filter.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+    import Selector from "./selector.svelte";
+
+    let answers: number[] = [];
+</script>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+<div class="filter">
+    <div class="filter__title">
+        <span class="material-symbols-outlined">tune</span>
+        <span class="filter__title__text">Filter</span>
+    </div>
+
+    <div class="filter__card">
+        <div class="filter__card__price">
+            <span class="title__text">Prix</span>
+            <Selector
+				data={[
+					"euro_symbol",
+					"euro_symbol",
+					"euro_symbol",
+					"euro_symbol",
+					"euro_symbol",
+				]}
+				rating_like={true}
+				bind:selected={answers[1]}
+			/>
+        </div>
+
+        <div class="filter__card__difficulty">
+            <span class="title__text">Difficulté</span>
+            <Selector
+                data={[
+                    "mood_bad",
+					"sentiment_neutral",
+					"sentiment_content",
+					"sentiment_calm",
+					"sentiment_satisfied",
+                ]}
+                rating_like={true}
+                bind:selected={answers[2]}
+            />
+        </div>
+
+        <div class="filter__card__tag">
+            <span class="title__text">Tags</span>
+            <Selector
+                data={[
+                ]}
+                rating_like={true}
+                bind:selected={answers[3]}
+            />
+        </div>
+    </div>
+</div>
+
+
+<style lang="scss">
+    .filter {
+        
+        .filter__title {
+            display: flex;
+            align-items: center;
+            margin-bottom: 1rem;
+
+            .material-symbols-outlined {
+                font-size: 2rem;
+                margin-right: 1rem;
+            }
+
+            .filter__title__text {
+                font-size: 1.5rem;
+                font-weight: 500;
+            }
+        }
+
+        .filter__card {
+            display: flex;
+            flex-direction: column;
+            border: 1px solid #e0e0e0;
+
+            width: 70%;
+
+            .filter__card__price,
+            .filter__card__difficulty,
+
+            .title__text {
+                font-size: 1.2rem;
+                font-weight: 500;
+            }
+        }
+    }
+</style>
--- frontend/src/routes/search/+page.svelte
+++ frontend/src/routes/search/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	import Filter from '../../components/filter.svelte';
+</script>
+
 <svelte:head>
 	<title>Recherche | PirateCook</title>
 </svelte:head>
 
 <h1>Permet de chercher des recherche selon SearchBar / Filter</h1>
+
+<Filter />
