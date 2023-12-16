9c485dcc92d61598f225c0a170e4575493418471
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2023-12-16 01:58:32 +0100

    UI addToPlaylist dropdown

---
+++ frontend/src/components/addToPlaylist.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    export let idRecipe: number;
+    
+    let isOpen = false;
+  
+    onMount(() => {
+      document.addEventListener('click', handleOutsideClick);
+      return () => {
+        document.removeEventListener('click', handleOutsideClick);
+      };
+    });
+  
+    function toggleDropdown() {
+      isOpen = !isOpen;
+      updateDropdownVisibility();
+    }
+  
+    function addToPlaylist(playlist: string) {
+      // Implement your logic to add to the selected playlist
+      console.log(`Added to playlist: ${playlist}`);
+
+      // Close the dropdown
+      toggleDropdown();
+    }
+  
+    function handleOutsideClick(event: Event) {
+      const target = event.target as HTMLElement;
+      if (!target.closest('.dropdown')) {
+        isOpen = false;
+      }
+    }
+
+    function updateDropdownVisibility() {
+        const dropdownContent = document.querySelector('.dropdown-content');
+        if (dropdownContent) {
+        dropdownContent.style.display = isOpen ? 'block' : 'none';
+        }
+    }
+  </script>
+  
+  <style lang="scss">
+    .dropdown {
+    }
+  
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      box-shadow: 0px rgba(0, 0, 0, 0.2);
+      z-index: 1;
+      
+    }
+  
+    .dropdown-item {
+      cursor: pointer;
+      /* reset all button properties */
+      background: none;
+	  border: none;
+	  padding: 1rem;
+	  cursor: pointer;
+    }
+  
+    .dropdown-item:hover {
+      background-color: #ddd;
+    }
+
+    button.material-symbols-rounded {
+		/* reset all button properties */
+		background: none;
+		border: none;
+		padding: 0;
+		cursor: pointer;
+		outline: inherit;
+	}
+  </style>
+  
+  <div class="dropdown">
+    <!-- Button to change isOpen to show or not the dropdown -->
+    <button on:click={toggleDropdown} class="material-symbols-rounded">playlist_add</button>
+
+    <div class="dropdown-content">
+      <form use:enhance method="post" action="?/addPlaylistRecipe">
+        <input hidden name="id" value={idRecipe} type="text" />
+          <div>
+            <button name="idPlaylist" value={1} class="dropdown-item">Playlist 1</button>
+          </div>
+          <div>
+            <button name="idPlaylist" value={2} class="dropdown-item">Playlist 2</button>
+          </div>
+          <div>
+            <button name="idPlaylist" value={3} class="dropdown-item">Playlist 3</button>
+          </div>
+      </form>
+    </div>
+  </div>
+  
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	import RecipeCard from "../components/recipeCard.svelte";
+	import type { RecipeDetail } from "$lib/api/recipe-types";
+
+
+	 // Mock data for demonstration
+	const mockRecipe: RecipeDetail = {
+		difficulty: 'easy',
+		images: ['image-url-1', 'image-url-2'],
+		quantity: '4 servings',
+		price: 'medium',
+		time: '30 minutes',
+		name: 'Delicious Recipe',
+		idRecipe: 1,
+	};
 </script>
 
 <svelte:head>
@@ -6,3 +20,15 @@
 </svelte:head>
 
 <h1>Wow that the homepage</h1>
+
+<div class="recipe__container">
+	<RecipeCard data={mockRecipe} />
+</div>
+
+<style lang='scss'>
+	.recipe__container {
+		display: flex;
+		justify-content: center;
+		width: 20%;
+	}
+</style>
