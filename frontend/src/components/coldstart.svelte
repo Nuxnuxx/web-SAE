fc7b33e7d3e5bfd86c8bb97b3ebfa9cc7d2358be
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2024-02-07 12:06:44 +0100

    fix coldstart with the swiper

--- frontend/src/components/coldstart.svelte
+++ frontend/src/components/coldstart.svelte
@@ -5,13 +5,14 @@
 	import { Price, Difficulty } from "$lib/api/recipe-types";
 	import Swiper2 from "./Swiper2.svelte";
 	import { coldstartLiked } from "../store";
+	import { redirect } from "@sveltejs/kit";
 
 	let currentQuestion = 0;
 	let answers: number[] = [];
 	let recipes: any[] = [];
 	let jsonRecipes: any;
 
-	async function getRecipesByPreferences() {
+	/*	async function getRecipesByPreferences() {
 		jsonRecipes = await getRecipes(
 			"",
 			Price[answers[1]],
@@ -24,12 +25,12 @@
 
 			recipes = jsonRecipes.result;
 		}
-	}
+	}*/
 </script>
 
 <main>
 	<div class="container">
-		<form method="post" action="?/coldstart">
+		<form method="post" action="?/endColdstart">
 			{#if currentQuestion === 0}
 				<h2
 					>Nous allons maintenant vous poser quelque
@@ -87,67 +88,16 @@
 					]}
 					bind:selected={answers[2]}
 				/>
-				<button
-					class="button"
-					on:click={() => {
-						getRecipesByPreferences();
-						currentQuestion++;
-					}}
-				>
+				<button class="button" on:click={() => {}} type="submit">
 					<span class="buttonnext__text">Suivant</span><span
 						class="material-symbols-rounded"
 					>
 						arrow_forward
 					</span>
 				</button>
-				<!--{:else if currentQuestion === 3}
-				<h2>
-					Et pour les <span class="highlight">épices</span> ?
-				</h2>
-				<Selector
-					data={["fire_truck", "local_fire_department", "block"]}
-					bind:selected={answers[3]}
-				/>
-				<button
-					class="button"
-					on:click={() => {
-						currentQuestion++;
-					}}
-				>
-					<span class="buttonnext__text">Suivant</span><span
-						class="material-symbols-rounded"
-					>
-						arrow_forward
-					</span>
-				</button>-->
-			{:else}
-				<!--<h2>
-					Pour finir, fait nous savoir ce que tu aimes en choisissant
-					<span class="highlight">au moins 3</span>
-					recettes.
-				</h2>-->
 
-				<h2>
-					Clique sur termine pour voir les recettes qui te
-					<span class="highlight">correspondent</span>
-				</h2>
-
-				<!-- faire le swiper selon les recipesbypreferences -->
-				<Swiper2 {recipes} />
-
-				<!-- en invisible, mettre les réponses pour les avoir dans le body -->
 				<input type="hidden" name="price" value={answers[1]} />
 				<input type="hidden" name="difficulty" value={answers[2]} />
-				<input type="hidden" name="liked" value={$coldstartLiked} />
-
-				<button
-					class="button"
-					on:click={() => {}}
-					type="submit"
-					disabled={$coldstartLiked.length < 3}
-				>
-					<span>Terminer</span>
-				</button>
 			{/if}
 		</form>
 	</div>
